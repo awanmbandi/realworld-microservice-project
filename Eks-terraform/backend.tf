3b8421baff9b9245588acdac82f118789e7eb67e
awanmbandi <awanmbandi@gmail.com>
2024-03-03 16:10:21 -0500

    updated project config

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "terraform-statefile-eks-dev-sec-ops" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
   }
 }
