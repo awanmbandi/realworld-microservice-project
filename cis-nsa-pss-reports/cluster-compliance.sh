9597ee9476fe8b7a21826afddafd1919bccded84
awanmbandi <awanmbandi@gmail.com>
2024-03-04 22:54:49 -0500

    added CIS/NSA cluster

--- cis-nsa-pss-reports/cluster-compliance.sh
+++ cis-nsa-pss-reports/cluster-compliance.sh
@@ -1,11 +1,11 @@
-# CIS Compliance benchmark report
+# CIS - Center for Internet Security Compliance Benchmark Report
 trivy k8s cluster --compliance=k8s-cis --report summary
 trivy k8s cluster --compliance=k8s-cis --report summary > trivy_cis_compliace_report.txt
 
-# NSA Compliance benchmark
+# NSA - National Security Agency Compliance Benchmark Report
 trivy k8s cluster --compliance=k8s-nsa --report all
 trivy k8s cluster --compliance=k8s-nsa --report all > trivy_nsa_compliace_report.txt
 
-# PSS - Pod Security Standards, Baseline benchmark
+# PSS - Pod Security Standards, Baseline Benchmark Report
 trivy k8s cluster --compliance=k8s-pss-baseline --report summary
 trivy k8s cluster --compliance=k8s-pss-baseline --report summary > trivy_pss_baseline_compliace_report.txt
